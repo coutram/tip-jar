f5d1691d9e178f7bb9c6045bc48ff0b0363829a7
coutram <outram@gmail.com>
2022-01-29 18:18:30 -0500

    initial commit of tip jar project

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/Utx42TapuK59Bo7j4jphzdPvkBB8HSVo',
+      accounts: [
+        '2a27aae5e1a2301df09a4ee76fbe3d9e70b7f2e8cc568be124dca6546162a393',
+      ],
+    },
+  },
+};
